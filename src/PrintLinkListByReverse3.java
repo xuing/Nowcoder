02f6f0c389e3aa6e5dbb3cd12728ed208a0dc681
xuing <lika@xuing.cn>
2018-04-12 15:22:48 +0800

    牛客网剑指Offer 第二题第三题

--- src/PrintLinkListByReverse3.java
+++ src/PrintLinkListByReverse3.java
@@ -1,23 +1,59 @@
+import org.junit.Test;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
-  @author Lika
-  @date 2018/04/12 14:35
-  Description:从尾到头打印链表
-  输入一个链表，从尾到头打印链表每个节点的值。
+ @author Lika
+ @date 2018/04/12 14:35
+ Description:从尾到头打印链表
+ 输入一个链表，从尾到头打印链表每个节点的值。
+ 运行时间：36ms
+ 占用内存：9164k
  */
 public class PrintLinkListByReverse3 {
     public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-        return null;
+        if (listNode == null) {
+            return new ArrayList<Integer>();
+        }
+        ListNode tempList = listNode;
+        int len = 1;
+        while (tempList.next != null) {
+            len++;
+            tempList = tempList.next;
+        }
+        Integer[] result = new Integer[len];
+        tempList = listNode;
+        while (len > 0) {
+            result[len - 1] = tempList.val;
+            tempList = tempList.next;
+            len--;
+        }
+        ArrayList<Integer> res = new ArrayList<>(Arrays.asList(result));
+        return res;
     }
 
-     public class ListNode {
-         int val;
-         ListNode next = null;
- 
-         ListNode(int val) {
-             this.val = val;
-         }
-     }
- 
+    @Test
+    public void test() {
+        ListNode list = new ListNode(1);
+        list.next = new ListNode(2);
+        list.next.next = new ListNode(3);
+
+        ArrayList<Integer> arrayList = printListFromTailToHead(null);
+        int i = 0;
+        while (i < arrayList.size()) {
+            System.out.println(arrayList.get(i));
+            i++;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
 }
--- src/ReplaceSpace2.java
+++ src/ReplaceSpace2.java
@@ -4,6 +4,7 @@
  * Description:替换空格
  * 请实现一个函数，将一个字符串中的空格替换成“%20”。
  * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+ *
  */
 public class ReplaceSpace2 {
     public String replaceSpace(StringBuffer str) {
@@ -27,4 +28,10 @@ public class ReplaceSpace2 {
     public static void main(String[] args) {
         System.out.println(new ReplaceSpace2().replaceSpace(new StringBuffer("We Are Happy.")));
     }
+
+    /**
+     * PS:
+     * java与C语言在字符串结束符上的区别
+     * https://blog.csdn.net/sszgg2006/article/details/49148189
+     */
 }
